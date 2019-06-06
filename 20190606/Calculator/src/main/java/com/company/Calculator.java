69febddcb2a841932c195173a98f182e2066e91a
Maria Sanchez <msanchez157@gmail.com>
2019-06-05 22:22:27 -0400

    Simple-Calculator

---
+++ 20190606/Calculator/src/main/java/com/company/Calculator.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Calculator {
+
+    /* Create a simple Calculator class with methods to perform the following operations:
+            add - Add two integers.
+            subtract - Subtract two integers.
+            multiply - Multiply two integers.
+            divide - Divide two integers.
+    */
+
+    // Add
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtract
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiply
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Divide
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+
+}
