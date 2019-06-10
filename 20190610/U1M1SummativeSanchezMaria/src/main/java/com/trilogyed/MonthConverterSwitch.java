b4518342d43b90244893758923f2ad92b64c703f
Maria Sanchez <msanchez157@gmail.com>
2019-06-09 20:46:05 -0400

    Month Converter Switch (Conditionals)

--- 20190610/U1M1SummativeSanchezMaria/src/main/java/com/trilogyed/MonthConverterSwitch.java
+++ 20190610/U1M1SummativeSanchezMaria/src/main/java/com/trilogyed/MonthConverterSwitch.java
@@ -7,7 +7,7 @@ Month Converter (Conditionals):
         -Print out the name of the month corresponding to the number typed in by the user.
         -Print out the following error message if the user enters a number less than 1 or greater than 12:
          "You have entered an invalid number. You must enter a number between 1 and 12. Goodbye."
-*/
+ */
 
 package com.trilogyed;
 
