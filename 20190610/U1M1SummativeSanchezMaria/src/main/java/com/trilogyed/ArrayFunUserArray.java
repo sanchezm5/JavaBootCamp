5d8482a061b466a7f7957fb8ea33622ef97a431e
Maria Sanchez <msanchez157@gmail.com>
2019-06-09 22:42:12 -0400

    Array Fun User Array (Arrays)

---
+++ 20190610/U1M1SummativeSanchezMaria/src/main/java/com/trilogyed/ArrayFunUserArray.java
@@ -0,0 +1,47 @@
+/*
+Array Fun (Arrays):
+    This solution must be contained in a Java file called ArrayFunUserArray. Your code must read 5 ints from the user,
+    put them in an array, and print the array elements to the screen.
+ */
+package com.trilogyed;
+
+import java.util.Scanner;
+
+public class ArrayFunUserArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Declare 5 variables to represent each integer
+        int num;
+        int counter = 0;
+        int[] arr = new int[5];
+
+        System.out.println("You will enter 5 integers. Let's begin!" + "\n");
+
+        do {
+            // Prompt the user to enter 4 more integers
+            System.out.print("Enter an integer: ");
+
+            // Read in the number and convert it to an integer
+            String userInput = scan.nextLine();
+            num = Integer.parseInt(userInput);
+
+            // Put the value into an array
+            arr[counter] = num;
+
+            // Each time the user enters an integer, increment the counter by 1
+            counter++;
+
+        } while (counter < 5);
+
+        // After the integers have been stored in the array, print them to the screen
+        System.out.print("You have entered: ");
+        for (int i = 0; i < arr.length; i++) {
+            if ( i < 4) {
+                System.out.print(arr[i] + ",");
+            } else {
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
